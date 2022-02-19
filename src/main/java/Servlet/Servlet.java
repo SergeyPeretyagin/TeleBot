d4ee76d213f5cce0ae31eb9181bfc7913fb469a6
Sergey Peretyagin <wodnyi33@yandex.ru>
2022-02-19 10:33:41 +0500

    commit 6

--- src/main/java/Servlet/Servlet.java
+++ src/main/java/Servlet/Servlet.java
@@ -1,30 +1,30 @@
-package Servlet;
-
-import Bot.Bot;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-
-public class Servlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("AAAAAAAAAAAAAAAAAAAAAAA");
-        try {
-            Bot.start();
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
-}
+//package Servlet;
+//
+//import Bot.Bot;
+//import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+//
+//public class Servlet extends HttpServlet {
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.getWriter().println("AAAAAAAAAAAAAAAAAAAAAAA");
+//        try {
+//            Bot.start();
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+//    }
+//}
