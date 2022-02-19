cbcbacede4f3fb6486162616e2d0d56ae79cc3b1
Sergey Peretyagin <wodnyi33@yandex.ru>
2022-02-19 10:26:36 +0500

    commit 4

--- src/main/java/Servlet/Servlet.java
+++ src/main/java/Servlet/Servlet.java
@@ -11,18 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/")
+
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("AAAAAAAAAAAAAAAAAAAAAAA");
         try {
             Bot.start();
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("AAAAAAAAAAAAAAAA");
     }
 
     @Override
